63d96286613fbe32803f43f09e393d7feda27759
Jessica <jessica.rodrialves@outlook.com>
2025-03-07 12:47:26 -0300

    implementação classe Motorista e MotoristaRepository

--- src/main/java/com/github/model/Motorista.java
+++ src/main/java/com/github/model/Motorista.java
@@ -1,5 +1,75 @@
 package com.github.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name= "tb_motoristas")
 public class Motorista {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@NotNull
+		private Long id;
+		
+		@NotBlank(message = "O atributo nome é obrigatorio")
+		@Size(min = 3, message = "O nome tem que ser maior que 3 letras")
+		private String nome;
+		
+		@NotBlank(message = "O atributo cnh é obrigatorio")
+		@Size(min = 9 , max = 11, message = "A cnh tem que ser maior que 9 e menor que 11")
+		private String cnh;
+		
+		@NotBlank(message = "O atributo placa é obrigatorio")
+		@Size(min = 7 , max = 7, message = "A placa tem que ter somente 7 caracteres")
+		private String placa;
+		
+		@NotBlank(message = "O atributo veiculo é obrigatorio")
+		@Size(max = 1000, message = "O veículo pode ter até 1000 caracteres")
+		private String veiculo;
+		
+		
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getCnh() {
+			return cnh;
+		}
+		public void setCnh(String cnh) {
+			this.cnh = cnh;
+		}
+		public String getPlaca() {
+			return placa;
+		}
+		public void setPlaca(String placa) {
+			this.placa = placa;
+		}
+		public String getVeiculo() {
+			return veiculo;
+		}
+		public void setVeiculo(String veiculo) {
+			this.veiculo = veiculo;
+		}
+		
+		
+		
+		
+		
+	
 
 }
--- src/main/java/com/github/repository/MotoristaRepository.java
+++ src/main/java/com/github/repository/MotoristaRepository.java
@@ -1,9 +1,14 @@
 package com.github.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.github.model.Motorista;
 
 public interface MotoristaRepository extends JpaRepository<Motorista, Long>{
+	
+	public List<Motorista> findAllByVeiculoContainingIgnoreCase(@Param("veiculo") String veiculo);
 
 }
