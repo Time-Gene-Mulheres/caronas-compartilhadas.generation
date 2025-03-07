4979fcd7f11204837e292c177f24c8adcd017545
Jessica <jessica.rodrialves@outlook.com>
2025-03-07 11:47:17 -0300

    Inclusão da estrutura MVC

---
+++ src/main/java/com/github/controller/MotoristaController.java
@@ -0,0 +1,5 @@
+package com.github.controller;
+
+public class MotoristaController {
+
+}
---
+++ src/main/java/com/github/repository/MotoristaRepository.java
@@ -0,0 +1,9 @@
+package com.github.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.model.Motorista;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Long>{
+
+}
